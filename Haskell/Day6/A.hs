6082fbf5b46ed0642cdf5c619cf06777ec32cdce
Malte Clement <malteclement@web.de>
2022-12-08 02:14:52 +0100

    minor change

--- Haskell/Day6/A.hs
+++ Haskell/Day6/A.hs
@@ -7,5 +7,5 @@ input :: Show a => (String -> a) -> IO ()
 input f  = readFile ".\\input.txt"  >>= print . f
 
 solve :: Int -> String -> Int
-solve marker s = marker + minimum [ i | i <- [0..length s - marker], let inf = take marker $ drop i s, nub inf == inf]
+solve marker s = marker + head  [ i | i <- [0..length s - marker], let inf = take marker $ drop i s, nub inf == inf]
   
